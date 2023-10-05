eaf03b463d0b9d4ce3d1c36343b086a7310c8e18
Raymondkingjnr <raymondnnaji660@gmail.com>
2023-10-05 04:36:34 +0100

    -short_circuit

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -4,6 +4,8 @@ const ShortCircuitOverview = () => {
   const [text, setText] = useState("");
   const [name, setName] = useState("Raymond");
 
+  // || == truth value , && == falsh value
+
   return <h2>{text || name ? "Raymond" : "Error"}</h2>;
 };
 export default ShortCircuitOverview;
